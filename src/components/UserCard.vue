ec3043086ff4b95be9f625d9d92742ec172d5c93
Victor Gbadebo <victorgbadebo10@gmail.com>
2024-08-26 09:44:38 -0700

    Update UserCard.vue

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -8,16 +8,13 @@
 </script>
 
 <template>
-<<<<<<< HEAD
   <!-- <li :class="$style['user - card']">{{ user.name }}: {{ user.website }}</li> -->
   <li class="user-card">{{ user.name }}: {{ user.website }}</li>
-=======
   <li class="user-card">
     <RouterLink :to="`/user/${user.name}`">
       {{ user.name }}: {{ user.website }}
     </RouterLink>
   </li>
->>>>>>> main
 </template>
 
 <style>
